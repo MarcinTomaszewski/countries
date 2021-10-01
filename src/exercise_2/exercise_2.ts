d0ee64efb6b5f6addb7502671e1b45a0b2a64601
Marcin Tomaszewski <marcin.tomaszewski7@wp.pl>
2021-10-01 14:54:36 +0200

    Add new properties to class.

--- src/exercise_2/exercise_2.ts
+++ src/exercise_2/exercise_2.ts
@@ -1,14 +1,18 @@
 import { CountryList, EuropeUnionCountries } from "../types";
 
 export class CountriesEuropeanUnion implements EuropeUnionCountries {
-  countryList: CountryList[];
-  constructor(countries: CountryList[]) {
-    this.countryList = countries;
-  }
+  constructor(private countryList: CountryList[]) { }
 
   result: CountryList[] = [];
   sumPopulation = 0;
 
+  getResultCountries(): CountryList[] {
+    return this.result;
+  }
+
+  getSumPopulation(): number {
+    return this.sumPopulation;
+  }
   europeanUnionCountries() {
     let countries = [];
     for (let i = 0; i < this.countryList.length; i++) {
@@ -57,49 +61,3 @@ export const checkHighPopulation = (
 
   return population > num;
 };
-
-
-// export const countriesEuropeanUnion = function (
-//   this: any,
-//   countries: CountryList[]
-// ): void {
-//   this.countryList = countries;
-//   this.result = null;
-
-//   this.europeanUnionCountries = function () {
-//     let countries = [];
-//     for (let i = 0; i < this.countryList.length; i++) {
-//       if (
-//         this.countryList[i].regionalBlocs &&
-//         this.countryList[i].regionalBlocs[0].acronym === "EU"
-//       ) {
-//         countries.push(this.countryList[i]);
-//       }
-//     }
-//     this.result = countries;
-//     return this;
-//   };
-
-//   this.countriesWithoutLetterA = function () {
-//     this.result = this.result.filter(
-//       (country: CountryList) => !country.name.includes("a")
-//     );
-//     return this;
-//   };
-
-//   this.sortByPopulation = function () {
-//     this.result = this.result.sort(
-//       (a: CountryList, b: CountryList) => b.population - a.population
-//     );
-
-//     return this;
-//   };
-
-//   this.sumCountriesPopulation = function () {
-//     this.result = this.result
-//       .slice(0, 5)
-//       .map((country: CountryList) => country.population)
-//       .reduce((a: number, b: number) => a + b);
-//     return this;
-//   };
-// };
