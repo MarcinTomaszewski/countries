81b90add2a6cd28484750e6ad33798195e8db2ef
Marcin Tomaszewski <marcin.tomaszewski7@wp.pl>
2021-10-06 13:57:45 +0200

    Add ServicesKeys and ServicesCountries.

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ export interface CountryList {
   name: string;
   population: number;
   regionalBlocs?: { acronym: string }[];
-  area?: number;
+  area: number;
 }
 
 export interface Storage {
@@ -27,12 +27,23 @@ export interface StorageServices {
 }
 
 export interface EuropeUnionCountries {
-  sumPopulation: number;
-  result: CountryList[];
-  getResultCountries(): CountryList[];
-  getSumPopulation(): number;
-  europeanUnionCountries(): this;
-  countriesWithoutLetterA(): this;
-  sortByPopulation(): this;
-  sumCountriesPopulation(): this;
+  europeanUnionCountries(region: string): this;
+  countriesWithoutLetterA(letter: string): this;
+  sortByPopulation(property: string): this;
+  sumCountriesPopulation(property: string): number;
+}
+
+export interface ServicesKeys {
+  regional: string;
+  letter: string;
+  property: string;
+  numCountries: number;
+  border: number;
+}
+
+export interface ServicesCountries {
+  getCountriesFromRegional(arr: CountryList[], key: string): CountryList[];
+  getCountriesWithoutLetter(arr: CountryList[], key: string): CountryList[];
+  sortByProperty(arr: CountryList[], key: string): CountryList[];
+  sumByProperty(arr: CountryList[], numCountries: number, property: string): number;
 }
